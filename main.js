d7adee6a325648b5911bb50430d80989a18a8787
NINGA142 <108669430+NINGA142@users.noreply.github.com>
2022-12-06 17:14:29 +0330

    Create main.js

---
+++ main.js
@@ -0,0 +1,528 @@
+var mahdode = 0
+var mahdodeD = 0
+
+function one()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m+"1"
+  
+  let p = document.getElementById("aval").innerHTML
+  document.getElementById("aval").innerHTML = p + "1"
+  
+  ++mahdode
+  if (mahdode > 5) {
+  document.getElementById("sfr").disabled = true;
+  }
+  if (mahdode > 4) {
+    document.getElementById("sfr1").disabled = true;
+  }
+  if (mahdode == 7) {
+    document.getElementById("numbersO").style.display="none"
+    document.getElementById("numbersD").style.display="block"
+    document.getElementById("error").style.display= "block"
+}
+}
+function two()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m+"2"
+  
+  let p = document.getElementById("aval").innerHTML
+  document.getElementById("aval").innerHTML = p + "2"
+  
+  ++mahdode
+if (mahdode > 5) {
+  document.getElementById("sfr").disabled = true;
+}
+if (mahdode > 4) {
+  document.getElementById("sfr1").disabled = true;
+}
+  if (mahdode == 7) {
+    document.getElementById("numbersO").style.display="none"
+    document.getElementById("numbersD").style.display="block"
+    document.getElementById("error").style.display= "block"
+}
+}
+function tri()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m+"3"
+  
+  let p = document.getElementById("aval").innerHTML
+  document.getElementById("aval").innerHTML = p + "3"
+  
+  ++mahdode
+if (mahdode > 5) {
+  document.getElementById("sfr").disabled = true;
+}
+if (mahdode > 4) {
+  document.getElementById("sfr1").disabled = true;
+}
+  if (mahdode == 7) {
+    document.getElementById("numbersO").style.display="none"
+    document.getElementById("numbersD").style.display="block"
+    document.getElementById("error").style.display= "block"
+}
+}
+function fur()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m+"4"
+  
+  let p = document.getElementById("aval").innerHTML
+  document.getElementById("aval").innerHTML = p + "4"
+  
+  ++mahdode
+if (mahdode > 5) {
+  document.getElementById("sfr").disabled = true;
+}
+if (mahdode > 4) {
+  document.getElementById("sfr1").disabled = true;
+}
+  if (mahdode == 7) {
+    document.getElementById("numbersO").style.display="none"
+    document.getElementById("numbersD").style.display="block"
+    document.getElementById("error").style.display= "block"
+}
+}
+function fiv()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m+"5"
+  
+  let p = document.getElementById("aval").innerHTML
+  document.getElementById("aval").innerHTML = p + "5"
+  
+  ++mahdode
+if (mahdode > 5) {
+  document.getElementById("sfr").disabled = true;
+}
+if (mahdode > 4) {
+  document.getElementById("sfr1").disabled = true;
+}
+  if (mahdode == 7) {
+    document.getElementById("numbersO").style.display="none"
+    document.getElementById("numbersD").style.display="block"
+    document.getElementById("error").style.display= "block"
+}
+}
+function six()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m+"6"
+  
+  let p = document.getElementById("aval").innerHTML
+  document.getElementById("aval").innerHTML = p + "6"
+  
+  ++mahdode
+if (mahdode > 5) {
+  document.getElementById("sfr").disabled = true;
+}
+if (mahdode > 4) {
+  document.getElementById("sfr1").disabled = true;
+}
+  if (mahdode == 7) {
+    document.getElementById("numbersO").style.display="none"
+    document.getElementById("numbersD").style.display="block"
+    document.getElementById("error").style.display= "block"
+}
+}
+function svn()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m+"7"
+  
+  let p = document.getElementById("aval").innerHTML
+  document.getElementById("aval").innerHTML = p + "7"
+  
+  ++mahdode
+if (mahdode > 5) {
+  document.getElementById("sfr").disabled = true;
+}
+if (mahdode > 4) {
+  document.getElementById("sfr1").disabled = true;
+}if (mahdode > 5) {
+  document.getElementById("sfr").disabled = true;
+}
+if (mahdode > 4) {
+  document.getElementById("sfr1").disabled = true;
+}
+  if (mahdode == 7) {
+    document.getElementById("numbersO").style.display="none"
+    document.getElementById("numbersD").style.display="block"
+    document.getElementById("error").style.display= "block"
+}
+}
+function has()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m + "8"
+  
+  let p = document.getElementById("aval").innerHTML
+  document.getElementById("aval").innerHTML = p + "8"
+  
+    ++mahdode
+if (mahdode > 5) {
+  document.getElementById("sfr").disabled = true;
+}
+if (mahdode > 4) {
+  document.getElementById("sfr1").disabled = true;
+}
+  if (mahdode == 7) {
+    document.getElementById("numbersO").style.display = "none"
+    document.getElementById("numbersD").style.display = "block"
+    document.getElementById("error").style.display = "block"
+  }
+}
+function nin()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m + "9"
+  
+  let p = document.getElementById("aval").innerHTML
+  document.getElementById("aval").innerHTML = p + "9"
+  
+    ++mahdode
+if (mahdode > 5) {
+  document.getElementById("sfr").disabled = true;
+}
+if (mahdode > 4) {
+  document.getElementById("sfr1").disabled = true;
+}
+  if (mahdode == 7) {
+    document.getElementById("numbersO").style.display = "none"
+    document.getElementById("numbersD").style.display = "block"
+    document.getElementById("error").style.display = "block"
+  }
+}
+function sfr()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m + "0"
+
+  let p = document.getElementById("aval").innerHTML
+  document.getElementById("aval").innerHTML = p + "0"
+
+    ++mahdode
+if (mahdode > 5) {
+  document.getElementById("sfr").disabled = true;
+}
+if (mahdode > 4) {
+  document.getElementById("sfr1").disabled = true;
+}
+  if (mahdode == 7) {
+    document.getElementById("numbersO").style.display = "none"
+    document.getElementById("numbersD").style.display = "block"
+    document.getElementById("error").style.display = "block"
+  }
+}
+function sfr1()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m + "00"
+
+  let p = document.getElementById("aval").innerHTML
+  document.getElementById("aval").innerHTML = p + "00"
+
+  ++mahdode
+  ++mahdode
+  if (mahdode > 5) {
+    document.getElementById("sfr").disabled = true;
+  }
+  if (mahdode > 4) {
+    document.getElementById("sfr1").disabled = true;
+  }
+  if (mahdode > 6) {
+    document.getElementById("numbersO").style.display = "none"
+    document.getElementById("numbersD").style.display = "block"
+    document.getElementById("error").style.display = "block"
+  }
+}
+function sfr2()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m + "000"
+  
+  let p = document.getElementById("aval").innerHTML
+  document.getElementById("aval").innerHTML = p + "000"
+  
+    ++mahdode
+    ++mahdode
+    ++mahdode
+    if (mahdode > 5) {
+      document.getElementById("sfr").disabled = true;
+    }
+    if (mahdode > 4) {
+      document.getElementById("sfr1").disabled = true;
+    }
+
+  if (mahdode == 7) {
+    document.getElementById("numbersO").style.display = "none"
+    document.getElementById("numbersD").style.display = "block"
+    document.getElementById("error").style.display = "block"
+  }
+}
+{
+function oneT()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m+"1"
+  
+  let p = document.getElementById("dovom").innerHTML
+  document.getElementById("dovom").innerHTML = p + "1"
+  
+  ++mahdodeD
+  if (mahdodeD == 7) {
+    document.getElementById("numbersT").style.display="none"
+    document.getElementById("numbersD").style.display="block"
+    document.getElementById("error").style.display= "block"
+}
+}
+function twoT()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m+"2"
+  
+  let p = document.getElementById("dovom").innerHTML
+  document.getElementById("dovom").innerHTML = p + "2"
+  
+  ++mahdodeD
+  if (mahdodeD == 7) {
+    document.getElementById("numbersT").style.display="none"
+    document.getElementById("numbersD").style.display="block"
+    document.getElementById("error").style.display= "block"
+}
+}
+function triT()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m+"3"
+  
+  let p = document.getElementById("dovom").innerHTML
+  document.getElementById("dovom").innerHTML = p + "3"
+  
+  ++mahdodeD
+  if (mahdode == 14) {
+    document.getElementById("numbersT").style.display="none"
+    document.getElementById("numbersD").style.display="block"
+    document.getElementById("error").style.display= "block"
+}
+}
+function furT()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m+"4"
+  
+  let p = document.getElementById("dovom").innerHTML
+  document.getElementById("dovom").innerHTML = p + "4"
+  
+  ++mahdodeD
+  if (mahdodeD == 7) {
+    document.getElementById("numbersT").style.display="none"
+    document.getElementById("numbersD").style.display="block"
+    document.getElementById("error").style.display= "block"
+}
+}
+function fivT()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m+"5"
+  
+  let p = document.getElementById("dovom").innerHTML
+  document.getElementById("dovom").innerHTML = p + "5"
+  
+  ++mahdodeD
+  if (mahdodeD == 7) {
+    document.getElementById("numbersT").style.display="none"
+    document.getElementById("numbersD").style.display="block"
+    document.getElementById("error").style.display= "block"
+}
+}
+function sixT()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m+"6"
+  
+  let p = document.getElementById("dovom").innerHTML
+  document.getElementById("dovom").innerHTML = p + "6"
+  
+  ++mahdodeD
+  if (mahdodeD == 7) {
+    document.getElementById("numbersT").style.display="none"
+    document.getElementById("numbersD").style.display="block"
+    document.getElementById("error").style.display= "block"
+}
+}
+function svnT()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m+"7"
+  
+  let p = document.getElementById("dovom").innerHTML
+  document.getElementById("dovom").innerHTML = p + "7"
+  
+  ++mahdodeD
+  if (mahdodeD == 7) {
+    document.getElementById("numbersT").style.display="none"
+    document.getElementById("numbersD").style.display="block"
+    document.getElementById("error").style.display= "block"
+}
+}
+function hasT()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m + "8"
+  
+  let p = document.getElementById("dovom").innerHTML
+  document.getElementById("dovom").innerHTML = p + "8"
+  
+    ++mahdodeD
+  if (mahdodeD == 7) {
+    document.getElementById("numbersT").style.display = "none"
+    document.getElementById("numbersD").style.display = "block"
+    document.getElementById("error").style.display = "block"
+  }
+}
+function ninT()
+{
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m +"9"
+  
+  let p = document.getElementById("dovom").innerHTML
+  document.getElementById("dovom").innerHTML = p+"9"
+  
+    ++mahdodeD
+  if (mahdodeD == 7) {
+    document.getElementById("numbersT").style.display = "none"
+    document.getElementById("numbersD").style.display = "block"
+    document.getElementById("error").style.display = "block"
+  }
+}
+}
+
+function zarb()
+{
+  document.getElementById("alamat").innerHTML="zarb"
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m+"×"
+  document.getElementById("zarb").disabled=true
+  document.getElementById("tagh").disabled=true
+  document.getElementById("menh").disabled=true
+  document.getElementById("jaam").disabled=true
+  document.getElementById("numbersT").style.display="block"
+  document.getElementById("numbersO").style.display="none"
+  document.getElementById("numbersD").style.display="none"
+  document.getElementById("error").style.display="none"
+}
+function tagh()
+{
+  document.getElementById("alamat").innerHTML = "tagh"
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m + "÷"
+  document.getElementById("zarb").disabled = true
+  document.getElementById("tagh").disabled = true
+  document.getElementById("menh").disabled = true
+  document.getElementById("jaam").disabled = true
+  document.getElementById("numbersT").style.display = "block"
+  document.getElementById("numbersO").style.display = "none"
+  document.getElementById("numbersD").style.display = "none"
+  document.getElementById("error").style.display = "none"
+}
+function menh()
+{
+  document.getElementById("alamat").innerHTML = "menh"
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m + "-"
+  document.getElementById("zarb").disabled = true
+  document.getElementById("tagh").disabled = true
+  document.getElementById("menh").disabled = true
+  document.getElementById("jaam").disabled = true
+  document.getElementById("numbersT").style.display = "block"
+  document.getElementById("numbersO").style.display = "none"
+  document.getElementById("numbersD").style.display = "none"
+  document.getElementById("error").style.display = "none"
+}
+function jaam()
+{
+  document.getElementById("alamat").innerHTML = "jaam"
+  let m = document.getElementById("mt").innerHTML
+  document.getElementById("mt").innerHTML = m + "+"
+  document.getElementById("zarb").disabled = true
+  document.getElementById("tagh").disabled = true
+  document.getElementById("menh").disabled = true
+  document.getElementById("jaam").disabled = true
+  document.getElementById("numbersT").style.display = "block"
+  document.getElementById("numbersO").style.display = "none"
+  document.getElementById("numbersD").style.display = "none"
+  document.getElementById("error").style.display = "none"
+}
+
+function mosavi()
+{
+  
+  var alamat = document.getElementById("alamat").innerHTML
+  
+  var aval = document.getElementById("aval").innerHTML
+  
+  var dovom = document.getElementById("dovom").innerHTML
+  
+  document.getElementById("error").style.display = "none"
+
+  
+  if (alamat == "zarb") {
+    document.getElementById("mt").innerHTML = aval*dovom
+  }else if (alamat == "tagh") {
+    document.getElementById("mt").innerHTML = aval/dovom
+  }else if (alamat == "jaam") {
+    document.getElementById("mt").innerHTML = Number(aval)+Number(dovom)
+  }else if (alamat=="menh") {
+    document.getElementById("mt").innerHTML = aval-dovom
+  }
+  let x = document.getElementById("mt").innerHTML
+  
+  document.getElementById("aval").innerHTML = x
+  
+document.getElementById("zarb").disabled = false;
+document.getElementById("tagh").disabled = false;
+document.getElementById("menh").disabled = false;
+document.getElementById("jaam").disabled = false;
+document.getElementById("numbersO").style.display = "block"
+document.getElementById("numbersD").style.display = "none"
+document.getElementById("numbersT").style.display = "none"
+document.getElementById("error").style.display = "none"
+document.getElementById("dovom").innerHTML="";
+  }
+function reset()
+{
+  document.getElementById("mt").innerHTML=""
+  mahdode = 0;
+  numbersD = 0;
+  document.getElementById("zarb").disabled = false;
+  document.getElementById("tagh").disabled = false;
+  document.getElementById("menh").disabled = false;
+  document.getElementById("jaam").disabled = false;
+  document.getElementById("sfr").disabled = false;
+  document.getElementById("sfr1").disabled = false;
+  document.getElementById("alamat").innerHTML="";
+  document.getElementById("aval").innerHTML="";
+  document.getElementById("dovom").innerHTML="";
+  document.getElementById("numbersO").style.display = "block"
+  document.getElementById("numbersD").style.display = "none"
+  document.getElementById("numbersT").style.display = "none"
+  document.getElementById("error").style.display = "none"
+
+}
+function change()
+{
+  let one = document.getElementById("input1").value
+  let tow = document.getElementById("input2").value
+  document.getElementById("all").style.backgroundImage=  
+  "linear-gradient(to bottom right, " +
+     one +
+     " 50%, " +
+     tow +
+     " 50%)";
+     if (one == "gray") {
+       document.getElementById("border").style.boxShadow="0 0 10px black"
+     }
+}
